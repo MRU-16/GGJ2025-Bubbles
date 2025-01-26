3abf6e68c608d5b5f78518c4b0bd3cb4d73e2d32
SamuelThomasVFS <gd75sam@vfs.com>
2025-01-25 17:05:25 -0800

    Improved menu layout

---
+++ Assets/Scripts/UI/MenuNav.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuNav : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> menuItemsToShow = new List<GameObject>();
+    [SerializeField] private List<GameObject> menuItemsToHide = new List<GameObject>();
+
+    public void ChangeView()
+    {
+        foreach (GameObject menuItem in menuItemsToShow)
+        {
+            menuItem.SetActive(true);
+        }
+        foreach (GameObject menuItem in menuItemsToHide)
+        {
+            menuItem.SetActive(false);
+        }
+    }
+}
